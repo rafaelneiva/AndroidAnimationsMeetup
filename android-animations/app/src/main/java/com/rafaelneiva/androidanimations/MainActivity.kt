af6614d27185cdc40559644d4875f1ae9917486a
rafaelneiva <rafael@zup.com.br>
2019-10-20 16:56:53 -0300

    page transformer

--- android-animations/app/src/main/java/com/rafaelneiva/androidanimations/MainActivity.kt
+++ android-animations/app/src/main/java/com/rafaelneiva/androidanimations/MainActivity.kt
@@ -1,13 +1,9 @@
 package com.rafaelneiva.androidanimations
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.tabs.TabLayout
-import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 import com.rafaelneiva.androidanimations.ui.main.SectionsPagerAdapter
 
 class MainActivity : AppCompatActivity() {
@@ -20,5 +16,29 @@ class MainActivity : AppCompatActivity() {
         viewPager.adapter = sectionsPagerAdapter
         val tabs: TabLayout = findViewById(R.id.tabs)
         tabs.setupWithViewPager(viewPager)
+        viewPager.offscreenPageLimit = 3
+
+//        viewPager.setPageTransformer(false) { page, position ->
+//
+//            // Zoom
+//            val minScale = 0.1f
+//            val scaleFactor = minScale + (1 - minScale) * (1 - Math.abs(position))
+//            page.pivotY = 0.5f * page.height
+//            page.pivotX = 0.5f * page.width
+//            page.scaleX = scaleFactor
+//            page.scaleY = scaleFactor
+//
+//            // Cube
+////            page.pivotX = if (position < 0f) page.width.toFloat() else 0f
+////            page.pivotY = page.width * 0.5f
+////            page.rotationY = 45f * position
+//
+//            // Rotation
+////            val rotation = 180f * position
+////            page.alpha = if(rotation > 90f || rotation < -90f) 0f else 1f
+////            page.pivotX = if (position < 0f) page.width.toFloat() else 0f
+////            page.pivotY = page.width * 0.5f
+////            page.rotationY = rotation
+//        }
     }
 }
